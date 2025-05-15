f14e8a91c69543567ecfc2304affe82c2211b5ec
mosza <punnavitp@gmail.com>
2025-05-15 22:40:54 +0700

    edit formleave

---
+++ app/api/admin/getalluser/route.tsx
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+import { initAdmin } from '@/firebase/firebaseAdmin'; // ฟังก์ชันที่คุณเขียนไว้
+
+export async function GET(req: Request) {
+  try {
+    const app = await initAdmin();
+    const db = app.firestore(); // ได้ Firestore จาก Admin SDK
+
+    const snapshot = await db.collection('Users').get();
+
+    const users = snapshot.docs.map((doc) => {
+      const data = doc.data();
+      return {
+        id: doc.id,
+        firstname: data.firstname,
+        lastname: data.lastname,
+        role: data.role,
+        username: data.username,
+      };
+    });
+    return NextResponse.json({ users }, { status: 200 });
+
+  } catch (err) {
+    console.error('[ADMIN_FIRESTORE_GET_ERROR]', err);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
